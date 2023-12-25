63bb12ece8d00a2cf75b7713d3b6919381374a73
edurodrigues0 <edusacramento9@gmail.com>
2023-12-25 19:35:15 -0300

    add prisma chambers repository

---
+++ src/repositories/prisma/prisma-chambers-repository.ts
@@ -0,0 +1,54 @@
+import { Chamber, Prisma } from '@prisma/client'
+import { ChambersRepository } from '../chambers'
+import { prisma } from '@/lib/prisma'
+
+export class PrismaChambersRepository implements ChambersRepository {
+  async create(data: Prisma.ChamberCreateInput) {
+    const chamber = await prisma.chamber.create({
+      data,
+    })
+
+    return chamber
+  }
+
+  async edit(id: string, data: Chamber) {
+    const chamber = await prisma.chamber.update({
+      where: { id },
+      data,
+    })
+
+    return chamber
+  }
+
+  async findByName(name: string, state: string) {
+    const chamber = await prisma.chamber.findFirst({
+      where: {
+        name,
+        state,
+      },
+    })
+    return chamber
+  }
+
+  async findByState(state: string, page: number) {
+    const chambers = await prisma.chamber.findMany({
+      where: {
+        state,
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return chambers
+  }
+
+  async findById(id: string) {
+    const chamber = await prisma.chamber.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return chamber
+  }
+}
