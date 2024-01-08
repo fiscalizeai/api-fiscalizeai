017c4f4a74d5316acefbd750c83556347e5adb2b
edurodrigues0 <edusacramento9@gmail.com>
2024-01-08 18:27:55 -0300

    delete factory chamber make-fetch-by-state

--- src/use-cases/factories/chambers/make-fetch-by-state-use-case.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaChambersRepository } from '@/repositories/prisma/prisma-chambers-repository'
-import { FetchByStateUseCase } from '@/use-cases/chambers/fetch-by-state'
-
-export function makeFetchByStateUseCase() {
-  const chambersRepository = new PrismaChambersRepository()
-  const useCase = new FetchByStateUseCase(chambersRepository)
-
-  return useCase
-}
