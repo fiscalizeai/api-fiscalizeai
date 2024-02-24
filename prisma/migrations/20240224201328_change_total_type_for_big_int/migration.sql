8e2a44d8db1d7f7e145800eec5757261d07f22f4
edurodrigues0 <edusacramento9@gmail.com>
2024-02-24 17:15:51 -0300

    Change total type for BigInt

---
+++ prisma/migrations/20240224201328_change_total_type_for_big_int/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "education_data" ALTER COLUMN "total" SET DATA TYPE BIGINT;
+
+-- AlterTable
+ALTER TABLE "health_data" ALTER COLUMN "total" SET DATA TYPE BIGINT;
+
+-- AlterTable
+ALTER TABLE "transport_data" ALTER COLUMN "total" SET DATA TYPE BIGINT;
