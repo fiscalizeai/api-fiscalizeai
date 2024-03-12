1a90027dd8c31a04530245aac19006b7f74fccd2
edurodrigues0 <edusacramento9@gmail.com>
2024-03-11 22:12:01 -0300

    add spending in annual route

--- src/http/controllers/dashboard/annual-transfers.ts
+++ src/http/controllers/dashboard/annual-transfers.ts
@@ -1,5 +1,6 @@
 import { prisma } from '@/lib/prisma'
 import { InvalidUserOrCityError } from '@/use-cases/errors/records/invalid-user-or-city'
+import { subMonths } from 'date-fns'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 // Definir um período de tempo de validade do cache em milissegundos
@@ -31,6 +32,11 @@ export async function annualTransfers(
       return reply.status(200).send({
         annualTransfers: cachedData.annualTransfers,
         totalTransfersInLastYear: cachedData.totalTransfersInLastYear,
+        totalSpendingWithChamber: cachedData.totalSpendingWithChamber,
+        totalSpendingWithEducation: cachedData.totalSpendingWithEducation,
+        totalSpendingWithHealth: cachedData.totalSpendingWithHealth,
+        totalSpendingWithTransport: cachedData.totalSpendingWithTransport,
+        totalAmountWithFinance: cachedData.totalAmountWithFinance,
         cached: true,
       })
     }
@@ -40,10 +46,149 @@ export async function annualTransfers(
       where: {
         city_id: city,
       },
+      take: 12,
+    })
+
+    const currentDate = new Date()
+    const currentYear = currentDate.getFullYear()
+    const currentMonth = currentDate.getMonth() + 1
+
+    // Calcula o ano e o mês há 6 meses atrás
+    const lastYearDate = subMonths(currentDate, 11)
+    const lastYear = lastYearDate.getFullYear()
+    const lastMonth = lastYearDate.getMonth() + 1
+
+    // Finance
+    const finance = await prisma.finance.findMany({
+      orderBy: [{ year: 'desc' }, { month: 'desc' }],
+      where: {
+        city_id: city,
+        AND: [
+          {
+            OR: [
+              {
+                AND: [{ year: currentYear }, { month: { lte: currentMonth } }],
+              },
+              {
+                AND: [{ year: lastYear }, { month: { gte: lastMonth } }],
+              },
+            ],
+          },
+        ],
+      },
+    })
+
+    // Health Records
+    const health = await prisma.health.findMany({
+      orderBy: [{ year: 'desc' }, { month: 'desc' }],
+      where: {
+        city_id: city,
+        AND: [
+          {
+            OR: [
+              {
+                AND: [{ year: currentYear }, { month: { lte: currentMonth } }],
+              },
+              {
+                AND: [{ year: lastYear }, { month: { gte: lastMonth } }],
+              },
+            ],
+          },
+        ],
+      },
+    })
+
+    // Education Records
+    const education = await prisma.education.findMany({
+      orderBy: [{ year: 'desc' }, { month: 'desc' }],
+      where: {
+        city_id: city,
+        AND: [
+          {
+            OR: [
+              {
+                AND: [{ year: currentYear }, { month: { lte: currentMonth } }],
+              },
+              {
+                AND: [{ year: lastYear }, { month: { gte: lastMonth } }],
+              },
+            ],
+          },
+        ],
+      },
+    })
+
+    // Chamber Records
+    const chamber = await prisma.chamber.findMany({
+      orderBy: [{ year: 'desc' }, { month: 'desc' }],
+      where: {
+        city_id: city,
+        AND: [
+          {
+            OR: [
+              {
+                AND: [{ year: currentYear }, { month: { lte: currentMonth } }],
+              },
+              {
+                AND: [{ year: lastYear }, { month: { gte: lastMonth } }],
+              },
+            ],
+          },
+        ],
+      },
+    })
+
+    // Transport Records
+    const transport = await prisma.transport.findMany({
+      orderBy: [{ year: 'desc' }, { month: 'desc' }],
+      where: {
+        city_id: city,
+        AND: [
+          {
+            OR: [
+              {
+                AND: [{ year: currentYear }, { month: { lte: currentMonth } }],
+              },
+              {
+                AND: [{ year: lastYear }, { month: { gte: lastMonth } }],
+              },
+            ],
+          },
+        ],
+      },
     })
 
     const totalTransfersInLastYear = annualTransfers.reduce(
       (acc, curr) => acc + parseInt(curr.value),
+      0,
+    )
+
+    const totalSpendingWithEducation = education.reduce(
+      (acc, curr) => acc + parseInt(curr.total.toString()),
+      0,
+    )
+
+    const totalSpendingWithChamber = chamber.reduce(
+      (acc, curr) => acc + parseInt(curr.total.toString()),
+      0,
+    )
+
+    const totalSpendingWithTransport = transport.reduce(
+      (acc, curr) => acc + parseInt(curr.total.toString()),
+      0,
+    )
+
+    const totalSpendingWithHealth = health.reduce(
+      (acc, curr) => acc + parseInt(curr.total.toString()),
+      0,
+    )
+
+    const totalAmountWithFinance = finance.reduce(
+      (acc, curr) =>
+        acc +
+        (parseInt(curr.iptu.toString()) +
+          parseInt(curr.iss.toString()) +
+          parseInt(curr.itbi.toString())),
       0,
     )
 
@@ -53,12 +198,22 @@ export async function annualTransfers(
       data: {
         annualTransfers,
         totalTransfersInLastYear,
+        totalSpendingWithHealth,
+        totalSpendingWithEducation,
+        totalSpendingWithTransport,
+        totalSpendingWithChamber,
+        totalAmountWithFinance,
       },
     }
 
     return reply.status(200).send({
       annualTransfers,
       totalTransfersInLastYear,
+      totalAmountWithFinance,
+      totalSpendingWithHealth,
+      totalSpendingWithEducation,
+      totalSpendingWithTransport,
+      totalSpendingWithChamber,
       cached: false,
     })
   } catch (error) {
